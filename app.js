b41f85d118fc5937dd6c44dd0f4ac47b06eacce4
evancmarion <77752834+evancmarion@users.noreply.github.com>
2026-08-16 11:32:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ const ARTWORKS = {
       description: [
         'Here I learned how to better work with delicate marks and relationships. I began the process with the large format painting, shown at Boats at Sea at Dawn. Afterwards, I wanted to continue developing the work, but without smothering the original composition.'
         'I navigated this by developing the work along a different axis, editing the canvas itself to refine compositions while keeping their surface undisturbed. This process produced the works below. ',
-        ]
+        ],
       image: 'images/canvas/big-painting.webp',
       heroImage: 'images/canvas/big-painting.webp',
       images: [
